c3460aba9a02acd2f43c572ceed5382a07b6ab74
Raisa Tabassum Kabir <2022100000032@seu.edu.bd>
2025-07-02 23:22:17 +0600

    "library-management-system"

--- src/main/java/bd/edu/seu/library_management_system/repository/ReturnBookRepository.java
+++ src/main/java/bd/edu/seu/library_management_system/repository/ReturnBookRepository.java
@@ -7,4 +7,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ReturnBookRepository extends JpaRepository<ReturnBook, String> {
 
+
+
 }
--- src/main/java/bd/edu/seu/library_management_system/service/RegistrationService.java
+++ src/main/java/bd/edu/seu/library_management_system/service/RegistrationService.java
@@ -24,4 +24,6 @@ public class RegistrationService {
         return registrationRepository.findAll();
     }
 
+
+
 }
