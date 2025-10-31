cc35499d1a550ada6ad8dfbbc2552113c6a37ae5
Raisa Tabassum Kabir <2022100000032@seu.edu.bd>
2025-11-01 00:49:31 +0600

    "library-management-system"

--- src/main/java/bd/edu/seu/library_management_system/service/DefaulterService.java
+++ src/main/java/bd/edu/seu/library_management_system/service/DefaulterService.java
@@ -2,10 +2,45 @@ package bd.edu.seu.library_management_system.service;
 
 
 
+import bd.edu.seu.library_management_system.model.Defaulter;
+import bd.edu.seu.library_management_system.model.IssuedBook;
+import bd.edu.seu.library_management_system.repository.IssuedBookRepository;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
 public class DefaulterService {
+    private final IssuedBookRepository issuedBookRepository;
+
+    public DefaulterService(IssuedBookRepository issuedBookRepository) {
+        this.issuedBookRepository = issuedBookRepository;
+    }
+        public List<Defaulter> findDefaulters() {
+        List<IssuedBook> allIssuedBooks = issuedBookRepository.findAll();
+        List<Defaulter> defaulters = new ArrayList<>();
+        LocalDate today = LocalDate.now();
+        for (IssuedBook book : allIssuedBooks) {
+            if (book.getReturnDate() != null && book.getReturnDate().isBefore(today)) {
+                long daysLate = ChronoUnit.DAYS.between(book.getReturnDate(), today);
+                long fine = daysLate * 10;
+                Defaulter defaulter = new Defaulter(
+                        book.getIssueDate(),
+                        book.getEmail(),
+                        fine,
+                        book.getIsbn(),
+                        book.getReturnDate(),
+                        book.getTitle()
+                );
+                defaulters.add(defaulter);
+            }
+        }
+        return defaulters;
+    }
+
 }
 
