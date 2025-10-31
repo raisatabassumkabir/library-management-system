5891314083d6dd9f04fb62245f28eb420de6bdba
Raisa Tabassum Kabir <2022100000032@seu.edu.bd>
2025-11-01 02:34:24 +0600

    "library-management-system"

--- src/main/java/bd/edu/seu/library_management_system/service/IssuedBookService.java
+++ src/main/java/bd/edu/seu/library_management_system/service/IssuedBookService.java
@@ -66,28 +66,6 @@ public class IssuedBookService {
             return issuedBookRepository.findAll();
         }
 
-//    public List<Defaulter> findDefaulters() {
-//        List<IssuedBook> allIssuedBooks = issuedBookRepository.findAll();
-//        List<Defaulter> defaulters = new ArrayList<>();
-//        LocalDate today = LocalDate.now();
-//        for (IssuedBook book : allIssuedBooks) {
-//            if (book.getReturnDate() != null && book.getReturnDate().isBefore(today)) {
-//                long daysLate = ChronoUnit.DAYS.between(book.getReturnDate(), today);
-//                long fine = daysLate * 10;
-//                Defaulter defaulter = new Defaulter(
-//                        book.getIssueDate(),
-//                        book.getEmail(),
-//                        fine,
-//                        book.getIsbn(),
-//                        book.getReturnDate(),
-//                        book.getTitle()
-//                );
-//                defaulters.add(defaulter);
-//            }
-//        }
-//        return defaulters;
-//    }
-
 
     //  Calculate Fine
     public long calculateFine(IssuedBook issuedBook) {
